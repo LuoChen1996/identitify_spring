937fea99fda60eb3cb94fd32e3c25f6875e6a39c
LuoChen1996 <luochen_cjlu@126.com>
2022-07-11 21:19:50 +0800

    自定义Spring循环依赖

---
+++ src/com/zhashu/CircleA.java
@@ -0,0 +1,39 @@
+//package com.zhashu;
+//
+//import spring.Autowired;
+//import spring.Component;
+//
+//@Component
+//public class CircleA {
+//
+//    @Autowired
+//    public CircleB circleB;
+//
+//    public String title = "TITLE CIRCLEA";
+//
+//    static {
+//        System.out.println("I am CircleA");
+//    }
+//
+//    public CircleB getCircleB() {
+//        return circleB;
+//    }
+//
+//    public void setCircleB(CircleB circleB) {
+//        this.circleB = circleB;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "CircleA{}";
+//    }
+//
+//}
---
+++ src/com/zhashu/CircleB.java
@@ -0,0 +1,39 @@
+//package com.zhashu;
+//
+//import spring.Autowired;
+//import spring.Component;
+//
+//@Component
+//public class CircleB {
+//
+//    @Autowired
+//    public CircleA circleA;
+//
+//    public String title = "TITLE CIRCLEB";
+//
+//    static {
+//        System.out.println("I am CircleB");
+//    }
+//
+//    public CircleA getCircleA() {
+//        return circleA;
+//    }
+//
+//    public void setCircleA(CircleA circleA) {
+//        this.circleA = circleA;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "CircleB{}";
+//    }
+//
+//}
